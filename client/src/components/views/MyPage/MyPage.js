d7f5cd8cd791b948378fb5b59e36d5e2f23376f2
jsoyun <syusmm233@gmail.com>
2021-11-21 11:08:10 +0900

    mypage productScreen css

--- client/src/components/views/MyPage/MyPage.js
+++ client/src/components/views/MyPage/MyPage.js
@@ -20,198 +20,188 @@ import MyPageAuth from "./Sections/MyPageAuth";
 ////////////////
 
 const MyPage = (props) => {
-    //   console.log("로컬 ", localStorage.getItem("cart"));
-    const dispatch = useDispatch();
-    const cart = useSelector((state) => state.cart);
-    const { cartItems } = cart;
+  //   console.log("로컬 ", localStorage.getItem("cart"));
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
 
-    const [Data, setData] = useState([{}]);
-    const [PostStates, setPostStates] = useState({});
-    const [lastIdx, setLastIdx] = useState(0);
-    const [userData1, setUserData1] = useState({
-        nickname: "",
-        userImage: "",
-        points: 0,
-    });
-    //const [userData2, setUserData2] = useState({ points: 0 });
+  const [Data, setData] = useState([{}]);
+  const [PostStates, setPostStates] = useState({});
+  const [lastIdx, setLastIdx] = useState(0);
+  const [userData1, setUserData1] = useState({
+    nickname: "",
+    userImage: "",
+    points: 0,
+  });
+  //const [userData2, setUserData2] = useState({ points: 0 });
 
-    useEffect(() => {
-        const fetchData = async (e) => {
-            try {
-                const res = await axios.get("/api/mypage");
+  useEffect(() => {
+    const fetchData = async (e) => {
+      try {
+        const res = await axios.get("/api/mypage");
 
-                const _Data = await res.data.userPosts.map(
-                    (rowData) => (
-                        setLastIdx(lastIdx + 1),
-                        {
-                            nickname: rowData.postedBy.nickname,
-                            photo: rowData.photo,
-                            compliteAuth: rowData.compliteAuth,
-                            wrongAuth: rowData.wrongAuth,
-                            comments: rowData.comments,
-                        }
-                    )
-                );
-
-                setUserData1({
-                    nickname: res.data.user.nickname,
-                    userImage: res.data.user.profileImage,
-                    points: res.data.user.points,
-                });
-                setPostStates(res.data.postsState);
-                return setData(Data.concat(_Data));
-            } catch (error) {}
-        };
-        fetchData();
-    }, []);
-    const qtyChangeHandler = (id, qty) => {
-        dispatch(addToCart(id, qty));
-    };
-
-    const removeHandler = (id) => {
-        dispatch(removeFromCart(id));
-    };
-
-    const getCartCount = () => {
-        return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
-    };
-
-    const getCartSubTotasl = () => {
-        return cartItems.reduce(
-            (price, item) => item.price * item.qty + price,
-            0
+        const _Data = await res.data.userPosts.map(
+          (rowData) => (
+            setLastIdx(lastIdx + 1),
+            {
+              nickname: rowData.postedBy.nickname,
+              photo: rowData.photo,
+              compliteAuth: rowData.compliteAuth,
+              wrongAuth: rowData.wrongAuth,
+              comments: rowData.comments,
+            }
+          )
         );
-    };
 
-    const myPoint = () => {
-        return cartItems.reduce(
-            (price, item) => item.price * item.qty + price,
-            0
-        );
+        setUserData1({
+          nickname: res.data.user.nickname,
+          userImage: res.data.user.profileImage,
+          points: res.data.user.points,
+        });
+        setPostStates(res.data.postsState);
+        return setData(Data.concat(_Data));
+      } catch (error) {}
     };
+    fetchData();
+  }, []);
+  const qtyChangeHandler = (id, qty) => {
+    dispatch(addToCart(id, qty));
+  };
 
-    const paymentBtn = (price) => {
-        axios
-            .put("/api/mypage/payment", { payPrice: price })
-            // .then((res) => res.json())
-            .then(alert("결제가 완료되었습니다."))
-            .catch((err) => {
-                console.log(err);
-            });
-        localStorage.removeItem("cart");
-        window.location.replace("/mypage");
-    };
+  const removeHandler = (id) => {
+    dispatch(removeFromCart(id));
+  };
 
-    const emptyProduct = () => {
-        alert("카트가 비어있습니다!");
-        window.location.replace("/product");
-    };
-    const exceedPoint = () => {
-        alert("현재 포인트를 초과하였습니다!");
-    };
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
 
-    return (
-        <div className="MyPage">
-            <div className="AuthBoard">
-                {/* /////////////////////////////// */}
-                {/* 현석 */}
-                <MyPageAuth
-                    photoData={Data}
-                    postState={PostStates}
-                    userData={userData1}
-                />
-                {/* /////////////////////////////// */}
-            </div>
-            <div className="cartscreen">
-                <div className="cartscreen_left">
-                    <div className="cartscreen_right">
-                        <div className="cartscreen_info">
-                            <div>
-                                <div>
-                                    <div
-                                        className="MyPage-cartscreen-subtitle"
-                                        style={{ borderBottomWidth: "0px" }}
-                                    >
-                                        현재 포인트{" "}
-                                    </div>{" "}
-                                    : <h2> {userData1.points}</h2>
-                                </div>
-                                <div>
-                                    <div
-                                        className="MyPage-cartscreen-subtitle"
-                                        style={{ borderBottomWidth: "0px" }}
-                                    >
-                                        결제 예정 금액{" "}
-                                    </div>{" "}
-                                    : <h2>{getCartSubTotasl()}</h2>
-                                </div>
-                                <div>
-                                    <div
-                                        className="MyPage-cartscreen-subtitle"
-                                        style={{ borderBottomWidth: "0px" }}
-                                    >
-                                        남은 포인트{" "}
-                                    </div>{" "}
-                                    :{" "}
-                                    {getCartSubTotasl() > userData1.points ? (
-                                        <h2>포인트 부족</h2>
-                                    ) : (
-                                        <h2>{userData1.points - myPoint()}</h2>
-                                    )}
-                                </div>
-                            </div>
+  const getCartSubTotasl = () => {
+    return cartItems.reduce((price, item) => item.price * item.qty + price, 0);
+  };
 
-                            <p>
-                                subtotal (
-                                <span className="MyPage-cartItemQty">
-                                    {getCartCount()}
-                                </span>
-                                ) items
-                            </p>
-                            {/* <p>결제예정금액 ${getCartSubTotasl().toFixed(2)}</p> */}
-                        </div>
-                        <div>
-                            {getCartSubTotasl() <= userData1.points ? (
-                                cartItems.length === 0 ? (
-                                    <button onClick={emptyProduct}>
-                                        Proceed To Checkout
-                                    </button>
-                                ) : (
-                                    <button
-                                        onClick={() => {
-                                            paymentBtn(myPoint());
-                                        }}
-                                    >
-                                        Proceed To Checkout
-                                    </button>
-                                )
-                            ) : (
-                                <button onClick={exceedPoint}>
-                                    Proceed To Checkout
-                                </button>
-                            )}
-                        </div>
-                    </div>
-                    <h2>Shopping Cart</h2>
-                    {cartItems.length === 0 ? (
-                        <div>
-                            Your cart is empty{" "}
-                            <Link to="/product">Go back</Link>
-                        </div>
-                    ) : (
-                        cartItems.map((item) => (
-                            <CartItem
-                                key={item.product}
-                                item={item}
-                                qtyChangeHandler={qtyChangeHandler}
-                                removeHandler={removeHandler}
-                            />
-                        ))
-                    )}
+  const myPoint = () => {
+    return cartItems.reduce((price, item) => item.price * item.qty + price, 0);
+  };
+
+  const paymentBtn = (price) => {
+    axios
+      .put("/api/mypage/payment", { payPrice: price })
+      // .then((res) => res.json())
+      .then(alert("결제가 완료되었습니다."))
+      .catch((err) => {
+        console.log(err);
+      });
+    localStorage.removeItem("cart");
+    window.location.replace("/mypage");
+  };
+
+  const emptyProduct = () => {
+    alert("카트가 비어있습니다!");
+    window.location.replace("/product");
+  };
+  const exceedPoint = () => {
+    alert("현재 포인트를 초과하였습니다!");
+  };
+
+  return (
+    <div className="MyPage">
+      <div className="AuthBoard">
+        {/* /////////////////////////////// */}
+        {/* 현석 */}
+        <MyPageAuth
+          photoData={Data}
+          postState={PostStates}
+          userData={userData1}
+        />
+        {/* /////////////////////////////// */}
+      </div>
+      <div className="cartscreen">
+        <div className="cartscreen_left">
+          <div className="cartscreen_right">
+            <div className="cartscreen_info">
+              <div>
+                <div>
+                  <div
+                    className="MyPage-cartscreen-subtitle"
+                    style={{ borderBottomWidth: "0px" }}
+                  >
+                    현재 포인트{" "}
+                  </div>{" "}
+                  : <h2> {userData1.points}</h2>
                 </div>
+                <div>
+                  <div
+                    className="MyPage-cartscreen-subtitle"
+                    style={{ borderBottomWidth: "0px" }}
+                  >
+                    결제 예정 금액{" "}
+                  </div>{" "}
+                  : <h2>{getCartSubTotasl()}</h2>
+                </div>
+                <div>
+                  <div
+                    className="MyPage-cartscreen-subtitle"
+                    style={{ borderBottomWidth: "0px" }}
+                  >
+                    남은 포인트{" "}
+                  </div>{" "}
+                  :{" "}
+                  {getCartSubTotasl() > userData1.points ? (
+                    <h2>포인트 부족</h2>
+                  ) : (
+                    <h2>{userData1.points - myPoint()}</h2>
+                  )}
+                </div>
+              </div>
+
+              <p>
+                subtotal (
+                <span className="MyPage-cartItemQty">{getCartCount()}</span>)
+                items
+              </p>
+              {/* <p>결제예정금액 ${getCartSubTotasl().toFixed(2)}</p> */}
             </div>
+            <div>
+              {getCartSubTotasl() <= userData1.points ? (
+                cartItems.length === 0 ? (
+                  <button onClick={emptyProduct}>Proceed To Checkout</button>
+                ) : (
+                  <button
+                    onClick={() => {
+                      paymentBtn(myPoint());
+                    }}
+                  >
+                    결제하기
+                  </button>
+                )
+              ) : (
+                <button onClick={exceedPoint}>Proceed To Checkout</button>
+              )}
+            </div>
+          </div>
+          <h2>장바구니</h2>
+          {cartItems.length === 0 ? (
+            <div>
+              장바구니가 비었습니다{" "}
+              <Link to="/product" className="GotoShop">
+                쇼핑하러 가기
+              </Link>
+            </div>
+          ) : (
+            cartItems.map((item) => (
+              <CartItem
+                key={item.product}
+                item={item}
+                qtyChangeHandler={qtyChangeHandler}
+                removeHandler={removeHandler}
+              />
+            ))
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
