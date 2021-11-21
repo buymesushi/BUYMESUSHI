bbd99f3e828467f789fdb0fc7794f66bece6ddd3
Seongjae <robbyl95@hanmail.net>
2021-11-21 12:38:48 +0900

    코드정리8

--- client/src/components/views/SliderPage/SliderPage.js
+++ client/src/components/views/SliderPage/SliderPage.js
@@ -1,9 +1,3 @@
-// 슬라이더 페이지
-// 이 안에는 사진과 정보를 담은 컴포넌트 하나랑
-// api를 보여주는 컴포넌트 하나가 있다.
-// cf) api 보여주는 컴포넌트 안에도 컴포넌트를 넣을 예정.
-// 사진도 넣고 설명도 넣고
-//(데이터 파일을 하나 만들어서 데이터 정보 때려박아넣고 불러오는걸로..styled component로 div 커스텀하기)
 import { useState } from "react";
 import {
   ArrowBackIosRounded,
